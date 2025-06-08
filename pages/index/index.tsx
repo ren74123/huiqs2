7ec17767db5ec398295aba6952e9618a72f60087
ren74123 <ren74123@gmail.com>
2025-06-08 13:16:43 +0800

    Add files via upload

---
+++ pages/index/index.tsx
@@ -0,0 +1,13 @@
+
+import { View } from '@tarojs/components'
+import AuthPage from '@/components/AuthPage'
+import './index.scss'
+import React from 'react';
+
+export default function Index() {
+  return (
+    <View className="index">
+      <AuthPage />
+    </View>
+  )
+}
